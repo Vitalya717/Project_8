3d08483151c993990f76f560389019f09af2870a
Vitalya Yakovlev <vitaalya717@mail.ru>
2024-08-16 10:17:10 +0300

    redesigned tests

--- src/test/java/ru/netology/services/SchemeServiceTest.java
+++ src/test/java/ru/netology/services/SchemeServiceTest.java
@@ -2,31 +2,17 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class SchemeServiceTest {
-    @Test
-    void restThreeMonthsYear() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/scheme.csv")
+    public void restThreeMonthsYear(int expected, int income, int expense, int threshold) {
         SchemeService service = new SchemeService();
 
-        int income = 10_000;
-        int expense = 3_000;
-        int threshold = 20_000;
-        long expected = 3;
-        long actual = service.calculate(income, expense, threshold);
+        int actual = service.calculate(income, expense, threshold);
+
         Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void restTwoMonthsYear() {
-        SchemeService service = new SchemeService();
-
-        int income = 100_000;
-        int expense = 60_000;
-        int threshold = 150_000;
-        long expected = 2;
-        long actual = service.calculate(income, expense, threshold);
-        Assertions.assertEquals(expected, actual);
-
     }
 }
